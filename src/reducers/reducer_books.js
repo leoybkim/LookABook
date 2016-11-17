86b1b9ffbf730a2c45ac8a234c2191c37dba358c
leoybkim <leoybkim@gmail.com>
2016-11-16 20:49:45 -0800

    Display more details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' },
+    { title: 'Javascript: The Good Parts', pages: 10000 },
+    { title: 'Harry Potter', pages: 42 },
+    { title: 'The Dark Tower', pages: 100 },
+    { title: 'Eloquent Ruby', pages: 0 },
   ]
 }
